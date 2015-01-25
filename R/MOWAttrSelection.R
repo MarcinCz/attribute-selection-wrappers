cd26c0b4e80a13d458f380c1d04e0d1cb21267c9
Pawel <kurowski.pawel1@wp.pl>
2015-01-25 21:00:17 +0100

    ustawienia parametrów first best

--- R/MOWAttrSelection.R
+++ R/MOWAttrSelection.R
@@ -7,7 +7,7 @@ selectAttributes <- function(trainData, trainLabels,  searchMethod='hill') {
   if(searchMethod=='hill') {
     hillClimbSearch(trainData, trainLabels)
   } else if(searchMethod=='bestFirst') {
-    bestFirstSearch(trainData, trainLabels)
+    bestFirstSearch(trainData, trainLabels, k=4, eps=0.05)
   }
 }
 
